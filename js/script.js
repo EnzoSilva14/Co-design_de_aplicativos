bfaef77ebed11df009e88ce4b6c2d833692284ba
Felipe <felipe.leventhal@gmail.com>
2023-12-05 20:26:17 -0300

    fim js

--- js/script.js
+++ js/script.js
@@ -12,16 +12,28 @@ document.addEventListener('DOMContentLoaded', function () {
         if (txt !== '') {
 
             section = document.createElement('section');
-            section.classList.add('botao3');
+            section.classList.add('botao35');
             main.appendChild(section);
+
+            div = document.createElement('div');
+            div.classList.add('linha2');
+            section.appendChild(div);
+
+            img = document.createElement('img');
+            img.src = 'img/noun-person-1192668.png';
+            img.classList.add('img1');
+            div.appendChild(img);
+
+            h2 = document.createElement('h2');
+            h2.textContent = 'Você';
+            div.appendChild(h2);
+
             h3 = document.createElement('h3');
             h3.classList.add('texto3');
             h3.textContent = txt;
             section.appendChild(h3);
+
             input.value = '';
         }
-
     });
-
-
 });
