0915a7a0f8848fcc31f53a439c8202b19bb33a03
Felipe <felipe.leventhal@gmail.com>
2023-12-05 20:02:01 -0300

    form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener('DOMContentLoaded', function () {
+
+    form = document.getElementById('form');
+    input = document.getElementById('input');
+    main = document.querySelector('main');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        txt = input.value;
+
+        if (txt !== '') {
+
+            main.createElement('div');
+        }
+
+    });
 
 });
